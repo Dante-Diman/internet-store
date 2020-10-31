5e976f5e0dde2f0d16668679fb5ac24a3afe0b68
Dante-Diman <45296855+Dante-Diman@users.noreply.github.com>
2020-10-31 20:09:46 +0700

    Updates

---
+++ precache-manifest.b4121fae728f5ba6c351780f24442e42.js
@@ -0,0 +1,258 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ec8f7e08fda04380c38131ba2d66d24",
+    "url": "/internet-store/index.html"
+  },
+  {
+    "revision": "ea81fee1c518878e2b88",
+    "url": "/internet-store/static/css/2.323389be.chunk.css"
+  },
+  {
+    "revision": "a858134270f9c89765f6",
+    "url": "/internet-store/static/css/main.4adf617c.chunk.css"
+  },
+  {
+    "revision": "ea81fee1c518878e2b88",
+    "url": "/internet-store/static/js/2.5c3cc99d.chunk.js"
+  },
+  {
+    "revision": "6ba7619993dff61ed196d50bfa8ff4f1",
+    "url": "/internet-store/static/js/2.5c3cc99d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a858134270f9c89765f6",
+    "url": "/internet-store/static/js/main.f653278c.chunk.js"
+  },
+  {
+    "revision": "685dcadc9fee4cdee09b",
+    "url": "/internet-store/static/js/runtime-main.02d89c5f.js"
+  },
+  {
+    "revision": "185d3af40521530a1119edaabca5c520",
+    "url": "/internet-store/static/media/1.185d3af4.jpg"
+  },
+  {
+    "revision": "41acdf816331640c9ad685a0c5e327f0",
+    "url": "/internet-store/static/media/1.41acdf81.jpg"
+  },
+  {
+    "revision": "568f0ec6e7ccbd3a7f355db8f531326b",
+    "url": "/internet-store/static/media/1.568f0ec6.jpg"
+  },
+  {
+    "revision": "814f89d6a75942e7dca3fb824e89fb91",
+    "url": "/internet-store/static/media/1.814f89d6.jpg"
+  },
+  {
+    "revision": "a1d9d4df938ce491f4a3415e16225930",
+    "url": "/internet-store/static/media/1.a1d9d4df.jpg"
+  },
+  {
+    "revision": "ce6833b8587cc4c53c40e4d8b441d63a",
+    "url": "/internet-store/static/media/1.ce6833b8.jpg"
+  },
+  {
+    "revision": "e2f87ca503e8381e7da1c72438658a3b",
+    "url": "/internet-store/static/media/1.e2f87ca5.jpg"
+  },
+  {
+    "revision": "db27a5b5e5b43b763fa951ddd5a8f1e7",
+    "url": "/internet-store/static/media/10.db27a5b5.jpg"
+  },
+  {
+    "revision": "18d83b9ff61c3b702898c77e8a599d77",
+    "url": "/internet-store/static/media/2.18d83b9f.jpg"
+  },
+  {
+    "revision": "20a00a3831d5207b4209d8aa38a0d29a",
+    "url": "/internet-store/static/media/2.20a00a38.jpg"
+  },
+  {
+    "revision": "8c59f4288d7c0648a3cf73498e34646a",
+    "url": "/internet-store/static/media/2.8c59f428.jpg"
+  },
+  {
+    "revision": "8f454b6c2da77aee155939ba222015eb",
+    "url": "/internet-store/static/media/2.8f454b6c.jpg"
+  },
+  {
+    "revision": "960805ae611fa681611d0257975db5e2",
+    "url": "/internet-store/static/media/2.960805ae.jpg"
+  },
+  {
+    "revision": "ce7529c7abc5346ebba278c3407270e1",
+    "url": "/internet-store/static/media/2.ce7529c7.jpg"
+  },
+  {
+    "revision": "fb93f1e060eb64d866bc2b936830ed19",
+    "url": "/internet-store/static/media/2.fb93f1e0.jpg"
+  },
+  {
+    "revision": "05677a1c678bee35f67615ba57bb36c1",
+    "url": "/internet-store/static/media/3.05677a1c.jpg"
+  },
+  {
+    "revision": "157a658845471c86fbe8f8edb1cab2ac",
+    "url": "/internet-store/static/media/3.157a6588.jpg"
+  },
+  {
+    "revision": "37bffd9ec104f2cb0ff6b40771acf450",
+    "url": "/internet-store/static/media/3.37bffd9e.jpg"
+  },
+  {
+    "revision": "4b4e3e501ff6da0ec39e136ae84209f0",
+    "url": "/internet-store/static/media/3.4b4e3e50.jpg"
+  },
+  {
+    "revision": "7c634423cda9af0d26cac0e590542316",
+    "url": "/internet-store/static/media/3.7c634423.jpg"
+  },
+  {
+    "revision": "86d7b8ab5b96e65aaa593f7dfb99fa64",
+    "url": "/internet-store/static/media/3.86d7b8ab.jpg"
+  },
+  {
+    "revision": "01083318d1063ea067a75e86f6b717d9",
+    "url": "/internet-store/static/media/4.01083318.jpg"
+  },
+  {
+    "revision": "359b14a7e2890005d334a689a13dd843",
+    "url": "/internet-store/static/media/4.359b14a7.jpg"
+  },
+  {
+    "revision": "b2190ab043eb8149198736951e35ce1f",
+    "url": "/internet-store/static/media/4.b2190ab0.jpg"
+  },
+  {
+    "revision": "c65f1492ca3f34ef518fa14b556c585b",
+    "url": "/internet-store/static/media/4.c65f1492.jpg"
+  },
+  {
+    "revision": "da157f860d593c20414e10f7d05427c5",
+    "url": "/internet-store/static/media/4.da157f86.jpg"
+  },
+  {
+    "revision": "024e41173bc543b62cf7f4ba16779060",
+    "url": "/internet-store/static/media/5.024e4117.jpg"
+  },
+  {
+    "revision": "8f7907fa0cee9992249184b6b4f40b20",
+    "url": "/internet-store/static/media/5.8f7907fa.jpg"
+  },
+  {
+    "revision": "a16680e231471aab1ab25553426dafb9",
+    "url": "/internet-store/static/media/5.a16680e2.jfif"
+  },
+  {
+    "revision": "aadb19dc176481bead2f16c6651a13b6",
+    "url": "/internet-store/static/media/5.aadb19dc.jpg"
+  },
+  {
+    "revision": "d794ac03373ea7ae467986dd2ab15981",
+    "url": "/internet-store/static/media/5.d794ac03.jpg"
+  },
+  {
+    "revision": "0d5473a02ad76df32e630cd5c4e9c621",
+    "url": "/internet-store/static/media/6.0d5473a0.jpg"
+  },
+  {
+    "revision": "985b757a831f9aadaa4e3ebc0d338899",
+    "url": "/internet-store/static/media/6.985b757a.jpg"
+  },
+  {
+    "revision": "fbb65403609cb8feb7e9a272a9460ecd",
+    "url": "/internet-store/static/media/7.fbb65403.jpg"
+  },
+  {
+    "revision": "06b48baad586fbecf2965f567ab5e0f6",
+    "url": "/internet-store/static/media/8.06b48baa.jpg"
+  },
+  {
+    "revision": "baa617df1eb6bb14dbaf79ed6b2d078b",
+    "url": "/internet-store/static/media/9.baa617df.jpg"
+  },
+  {
+    "revision": "dd97d31733ccc59965b53584af389b31",
+    "url": "/internet-store/static/media/Thin_Left_Down_Arrow.dd97d317.svg"
+  },
+  {
+    "revision": "2f12242375edd68e9013ecfb59c672e9",
+    "url": "/internet-store/static/media/fa-brands-400.2f122423.svg"
+  },
+  {
+    "revision": "8300bd7f30e0a313c1d772b49d96cb8e",
+    "url": "/internet-store/static/media/fa-brands-400.8300bd7f.ttf"
+  },
+  {
+    "revision": "ad527cc5ec23d6da66e8a1d6772ea6d3",
+    "url": "/internet-store/static/media/fa-brands-400.ad527cc5.woff"
+  },
+  {
+    "revision": "e2ca6541bff3a3e9f4799ee327b28c58",
+    "url": "/internet-store/static/media/fa-brands-400.e2ca6541.eot"
+  },
+  {
+    "revision": "f075c50f89795e4cdb4d45b51f1a6800",
+    "url": "/internet-store/static/media/fa-brands-400.f075c50f.woff2"
+  },
+  {
+    "revision": "3602b7e8b2cb1462b0bef9738757ef8a",
+    "url": "/internet-store/static/media/fa-regular-400.3602b7e8.svg"
+  },
+  {
+    "revision": "3c6879c4f342203d099bdd66dce6d396",
+    "url": "/internet-store/static/media/fa-regular-400.3c6879c4.woff"
+  },
+  {
+    "revision": "49f00693b0e5d45097832ef5ea1bc541",
+    "url": "/internet-store/static/media/fa-regular-400.49f00693.ttf"
+  },
+  {
+    "revision": "4a74738e7728e93c4394b8604081da62",
+    "url": "/internet-store/static/media/fa-regular-400.4a74738e.woff2"
+  },
+  {
+    "revision": "b01516c1808be557667befec76cd6318",
+    "url": "/internet-store/static/media/fa-regular-400.b01516c1.eot"
+  },
+  {
+    "revision": "205f07b3883c484f27f40d21a92950d4",
+    "url": "/internet-store/static/media/fa-solid-900.205f07b3.ttf"
+  },
+  {
+    "revision": "4451e1d86df7491dd874f2c41eee1053",
+    "url": "/internet-store/static/media/fa-solid-900.4451e1d8.woff"
+  },
+  {
+    "revision": "664de3932dd6291b4b8a8c0ddbcb4c61",
+    "url": "/internet-store/static/media/fa-solid-900.664de393.svg"
+  },
+  {
+    "revision": "8ac3167427b1d5d2967646bd8f7a0587",
+    "url": "/internet-store/static/media/fa-solid-900.8ac31674.eot"
+  },
+  {
+    "revision": "8e1ed89b6ccb8ce41faf5cb672677105",
+    "url": "/internet-store/static/media/fa-solid-900.8e1ed89b.woff2"
+  },
+  {
+    "revision": "6d63a7b11eaa1810b19c71a5e8c858ed",
+    "url": "/internet-store/static/media/facebook_ico.6d63a7b1.svg"
+  },
+  {
+    "revision": "11dcb5fa7f811628b931541588bdf182",
+    "url": "/internet-store/static/media/instagram_ico.11dcb5fa.svg"
+  },
+  {
+    "revision": "ea97e7bcb33125ea25c6198febf764a2",
+    "url": "/internet-store/static/media/logo.ea97e7bc.svg"
+  },
+  {
+    "revision": "02d7d0a5f5b654894b06139e2f98e562",
+    "url": "/internet-store/static/media/twit-ico-offer.02d7d0a5.svg"
+  },
+  {
+    "revision": "06cc66a873824a6c3c95d37c016c559b",
+    "url": "/internet-store/static/media/twitter_ico.06cc66a8.svg"
+  }
+]);
