a58f0debeb311bf5fbc10b5180cbcf1bb36393c5
Дмитрий Кононов <45296855+Dante-Diman@users.noreply.github.com>
2021-10-06 17:53:45 +0700

    mobile menu
    adaptation

--- src/Components/Navbar/Login.jsx
+++ src/Components/Navbar/Login.jsx
@@ -17,7 +17,7 @@ export default class Login extends React.Component {
                     
         <div className={style.login_block}>
             <ul >
-                <li className={style.navItem} onClick={this.openLoginMenu}>Login</li>
+                <li className={style.navItem} onClick={this.openLoginMenu}>login</li>
                 <li><NavLink exact to="/internet-store/catalog/basket" activeClassName={style.activeLink} className={style.navItem}><i class="fa fa-shopping-cart" aria-hidden="true"></i></NavLink></li>
             </ul>
         </div>
